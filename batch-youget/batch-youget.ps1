ba2e5adf697b1444a9b4d7078b5cda77f4d02c30
KnightReigh <12376953+knightReigh@users.noreply.github.com>
2018-03-03 03:03:25 -0600

    Add files via upload

---
+++ batch-youget/batch-youget.ps1
@@ -0,0 +1,37 @@
+chcp 65001
+
+
+foreach($line in Get-Content -Encoding UTF8 .\vlist.txt) {
+    Copy-Item vlist.txt vlist-old.txt
+
+    if ($line -match "https.*") {
+        Write-Output $line
+        if (!(Test-Path -Path Downloaded)) {
+            New-Item -ItemType directory -Path Downloaded
+        }
+        while ($counter -ne 5) {
+            Try{
+                $json = you-get.exe --json --format=mp4 $line | ConvertFrom-Json
+                if ([int]($json.'streams'.'mp4'.'size') -lt 100000000) {
+                    you-get.exe -o Downloaded --format=mp4 $line
+                    if ($LASTEXITCODE -match 1) {
+                        continue
+                    }
+                    else {
+                        break
+                    }
+                }
+                else {
+                    break
+                }
+            }
+            Catch{
+                continue
+            }
+        }
+    }
+
+    get-content -encoding UTF8 vlist.txt | Select-Object -skip 2 | set-content -encoding UTF8 "tmp.txt"
+    Move-Item tmp.txt vlist.txt -Force
+}
+
